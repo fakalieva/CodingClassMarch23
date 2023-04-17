fb19041ad7e3d7965e4007f422d076a72d597a3d
Rositsa Fakalieva <rossitza.fakalieva@telerik.com>
2023-04-17 20:43:11 +0300

    Added javascript for navigation and form

--- ProjectMarch/js/script.js
+++ ProjectMarch/js/script.js
@@ -26,3 +26,46 @@ var alllinks = document.querySelectorAll("a:link");
 for (var i = 0; i < alllinks.length; i++) {
     ScrollSmooth(alllinks[i]);
 }
+
+
+var openEl = document.getElementsByName("menu-outline")[0];
+var closeEl = document.getElementsByName("close-outline")[0];
+closeEl.classList.toggle("hideButton");
+const headerEl = document.querySelector(".header");
+var btnEl = document.querySelector(".btn-mobile-nav");
+btnEl.addEventListener("click",
+
+    function () {
+
+
+        var mainNav = document.getElementById("main-nav");
+        closeEl.classList.toggle("hideButton");
+        openEl.classList.toggle("hideButton");
+        mainNav.classList.toggle("nav-open");
+
+    });
+/*
+function toggleClass(link) {
+
+    link.classList.toggle("nav-close");
+}
+
+const headerEl = document.querySelector(".header");
+
+
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const allIcons = document.querySelectorAll(".icon-mobile-nav");
+
+
+btnNavEl.addEventListener("click", function () {
+
+    allIcons.forEach(toggleClass);
+    headerEl.classList.toggle("nav-open");
+});
+
+const closeButton = document.getElementsByName("close-outline")[0];
+closeButton.classList.toggle("nav-close");
+//const sectionHeroEl = document.querySelector(".section-hero");
+
+*/
